71f50eb38812fe45ca5d1940c9c68f0dbab7577a
Cody Denniston <dennistonc4817@gapps.bcsds.org>
2020-04-09 13:14:46 -0500

    added dotenv

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config(); //reads from env file
+
 const server = require('./server.js');
-port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
